35d92dfbabac308e612dfec04f3959e113f67b6c
BigMoonTech <moonynotsunny@gmail.com>
2021-07-14 19:06:38 -0400

    Update to project folder
    Include Bootstrap 5.0.2, Updated templates folder structure, index and about pages created.

--- pypi_org/app.py
+++ pypi_org/app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+
+
+def get_packages():
+    return [
+        {'name': 'flask', 'version': '1.2.3'},
+        {'name': 'sqlalchemy', 'version': '2.0'},
+        {'name': 'retard', 'version': '3.8'}
+    ]
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    test_packages = get_packages()
+    return flask.render_template('home/index.html', packages=test_packages)
+
+
+@app.route('/about')
+def about():
+    return flask.render_template('home/about.html')
 
 
 if __name__ == '__main__':

808cddd4db1c0188fad5e65a040407e5d32780a0
BigMoonTech <moonynotsunny@gmail.com>
2022-06-12 17:36:12 -0400

    Add an auditing table to the db
    Another commit to experiment with the alembic migration tool. Added an auditing table to the db using alembic migrations.

--- pypi_org/data/__all_models.py
+++ pypi_org/data/__all_models.py
@@ -3,6 +3,8 @@
 # the need to muck around in the global init.
 
 # noinspection PyUnresolvedReferences
+import pypi_org.data.audit
+# noinspection PyUnresolvedReferences
 import pypi_org.data.downloads
 # noinspection PyUnresolvedReferences
 import pypi_org.data.languages
